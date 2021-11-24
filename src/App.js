e31b7cedd24e382092b1d089d3b8c86b941676c3
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-24 13:55:59 -0500

    remove task

--- src/App.js
+++ src/App.js
@@ -1,83 +1,4 @@
-/* import React, { Component } from "react";
-
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false },
-      ],
-      newTask: "",
-      error: false,
-    };
-  }
-  newTaskHandler = (e) => {
-    this.setState({
-      error: false,
-      newTask: e.target.value,
-    });
-    e.target.value = "";
-  };
-  submitHandler = (e) => {
-    e.preventDefault();
-    if (this.state.newTask.trim() !== "") {
-      this.setState({
-        tasks: [
-          ...this.state.tasks,
-          {
-            id: this.state.tasks.length + 1,
-            name: this.state.newTask,
-            done: false,
-          },
-        ],
-        newTask: "",
-      });
-    }
-    this.setState({ error: true });
-  };
-  toggleHandler = (e) => {
-    const target = this.state.tasks.find((i) => i.id == e.target.id);
-    const targetDone = target.done;
-    this.setState({
-      task: [(target.done = !targetDone), ...this.state.tasks],
-    });
-  };
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task) => (
-              <li
-                className={`${task.done ? "done" : ""}`}
-                key={task.id}
-                id={task.id}
-                onClick={this.toggleHandler}
-              >
-                {task.name}
-              </li>
-            ))}
-          </ul>
-          <form onSubmit={this.submitHandler}>
-            <input
-              type="text"
-              id="new-task"
-              placeholder="Ingresa una tarea y oprime Enter"
-              value={this.state.newTask}
-              onChange={this.newTaskHandler}
-              className={`${this.state.error ? "error" : ""}`}
-            />
-          </form>
-        </div>
-      </div>
-    );
-  }
-} */
 
 import React, { useState } from "react";
 
@@ -111,9 +32,12 @@ const App = () => {
       setError(true);
     }
   };
+  const removeHandler = (e) => {
+    setTasks(tasks.filter((i) => i.id !== parseInt(e.target.id, 0)));
+  };
 
   const toggleHandler = (e) => {
-    const target = tasks.find((i) => i.id == e.target.id);
+    const target = tasks.find((i) => i.id === parseInt(e.target.id, 0));
     const targetIndex = tasks.indexOf(target);
     let arrCopy = [...tasks];
     arrCopy[targetIndex].done = !arrCopy[targetIndex].done;
@@ -125,13 +49,17 @@ const App = () => {
         <h3>Por hacer:</h3>
         <ul className="todo">
           {tasks.map((task) => (
-            <li
-              className={`${task.done ? "done" : ""}`}
-              key={task.id}
-              id={task.id}
-              onClick={toggleHandler}
-            >
-              {task.name}
+            <li key={task.id}>
+              <span
+                className={`${task.done ? "done" : ""}`}
+                id={task.id}
+                onClick={toggleHandler}
+              >
+                {task.name}
+              </span>
+              <button onClick={removeHandler} id={task.id}>
+                x
+              </button>
             </li>
           ))}
         </ul>
